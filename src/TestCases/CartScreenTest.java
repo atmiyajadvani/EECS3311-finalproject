ef5aa21dd15e08f364bcdec2488b92b8a84bd573
farhan-a3 <farhan.a3hd@gmail.com>
2024-04-03 22:49:53 -0400

    junit 5 fix

--- src/TestCases/CartScreenTest.java
+++ src/TestCases/CartScreenTest.java
@@ -1,7 +1,7 @@
 package TestCases;
 import com.sun.xml.internal.ws.wsdl.writer.document.Import;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 import Backend.Item;
 import Backend.CartScreenBackEnd;
